18a86f589261262f51b2d4edab06f7fe9fcca386
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-09 03:20:34 +0400

    pl506 sensor id fix

--- pl506/pl506.component.js
+++ pl506/pl506.component.js
@@ -8,67 +8,61 @@ angular.module('pl506', []).component('pl506', {
             this.TT0101 = "";
             let self = this;
 
-            this.pl506Changer = function(crId) {
+            this.pl506Changer = function (crId) {
                 $window.location.href = "#!/pl506/";
                 console.log($window.location.href);
             };
 
 
-
             this.reload = function () {
+                self.timestamp = new Date();
+                $http
+                    .get("php-db-conn/np04cachedvals.php?elemName=pl506")
+                    .then(function (result) {
+                        const res = result.data;
+                        console.log(res);
+                        self.NP04_MHT0100AI = res["47878785489690"][0];
+                        self.NP04_TT0100AI = res["47878802266906"][0];
+                        self.NP04_PT0106AI = res["47878819044122"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_Status_On = res["47873534198810"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_MeasurementSenseVoltage = res["47873534219290"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_MeasurementCurrent = res["47873534219546"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_Status_On = res["47873601307674"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_MeasurementSenseVoltage = res["47873601328154"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_MeasurementCurrent = res["47873601328410"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_Status_On = res["47873685193754"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_MeasurementSenseVoltage = res["47873685214234"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_MeasurementCurrent = res["47873685214490"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_Status_On = res["47875547464730"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_MeasurementSenseVoltage = res["47875547485210"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_MeasurementCurrent = res["47875547485466"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_Status_On = res["47875614573594"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_MeasurementSenseVoltage = res["47875614594074"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_MeasurementCurrent = res["47875614594330"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_Status_On = res["47875698459674"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_MeasurementSenseVoltage = res["47875698480154"][0];
+                        self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_MeasurementCurrent = res["47875698480410"][0];
 
-                $http.get("php-db-conn/cachedVals.conn.php?elemId=pl506").then(function (resultArr) {
+                    });
 
-                    let rArr = [];
-                    let resjson = angular.toJson(resultArr.data);
-                    let res = JSON.parse(resjson);
-                    for (let i = 0; i < res.length; i++) {
-                        rArr.push(JSON.parse(res[i]));
-                    }
-
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_Status_On = rArr[0];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_MeasurementSenseVoltage = rArr[1];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel0_MeasurementCurrent = rArr[2];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_MeasurementSenseVoltage = rArr[3];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_Status_On = rArr[4];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel1_MeasurementCurrent = rArr[5];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_Status_On = rArr[6];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_MeasurementSenseVoltage = rArr[7];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK4_Channel2_MeasurementCurrent = rArr[8];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_MeasurementSenseVoltage = rArr[9];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_Status_On = rArr[10];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel0_MeasurementCurrent = rArr[11];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_MeasurementSenseVoltage = rArr[12];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_Status_On = rArr[13];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel1_MeasurementCurrent = rArr[14];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_Status_On = rArr[15];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_MeasurementSenseVoltage = rArr[16];
-                    self.NP04_DCS_01_Wiener_CE_PL506_RACK6_Channel2_MeasurementCurrent = rArr[17];
-                    self.NP04_MHT0100AI = rArr[18];
-                    self.NP04_TT0100AI = rArr[19];
-                    self.NP04_PT0106AI = rArr[20];
-
-                    console.log("interval occured");
-                    self.timestamp = rArr[rArr.length-1] * 1000;
-            });
             };
 
             this.promise;
 
             this.reload();
 
-            $scope.start = function() {
+            $scope.start = function () {
                 $scope.stop();
 
                 self.promise = $interval(self.reload, 60000);
             };
 
-            $scope.stop = function() {
+            $scope.stop = function () {
                 $interval.cancel(self.promise);
             };
             $scope.start();
 
-            $scope.$on('$destroy', function() {
+            $scope.$on('$destroy', function () {
                 $scope.stop();
             });
         }]
