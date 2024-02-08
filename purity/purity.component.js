8818f03b70f519497972221a903cd6d9f4cafd27
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-09 02:55:21 +0400

    purity sensor id fix

--- purity/purity.component.js
+++ purity/purity.component.js
@@ -8,89 +8,86 @@ angular.module('purity', []).component('purity', {
 
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=purity").then(function (resultArr) {
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=purity")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
 
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
+                    self.NP04_DCS_01_PrM0_PrM_V_A_Set = res["47928748018458"][0];
+                    self.NP04_DCS_01_PrM0_PrM_V_A = res["47928748018970"][0];
+                    self.NP04_DCS_01_PrM0_PrM_I_A = res["47928748019738"][0];
+                    self.NP04_DCS_01_PrM0_PrM_V_C_Set = res["47928748018714"][0];
+                    self.NP04_DCS_01_PrM0_PrM_V_C = res["47928748019226"][0];
+                    self.NP04_DCS_01_PrM0_PrM_I_C = res["47928748023578"][0];
+                    self.NP04_DCS_01_PrM0_PrM_corrected_charge_ratio = res["47928748016154"][0];
+                    self.NP04_DCS_01_PrM0_PrM_corrected_e_lifetime = res["47928748016410"][0];
+                    self.NP04_DCS_01_PrM0_PrM_Q_C = res["47928748016922"][0];
+                    self.NP04_DCS_01_PrM0_PrM_T_C = res["47928748017178"][0];
+                    self.NP04_DCS_01_PrM0_PrM_BL_C = res["47928748017434"][0];
+                    self.NP04_DCS_01_PrM0_PrM_Q_A = res["47928748017690"][0];
+                    self.NP04_DCS_01_PrM0_PrM_T_A = res["47928748017946"][0];
+                    self.NP04_DCS_01_PrM0_PrM_BL_A = res["47928748018202"][0];
+                    self.NP04_DCS_01_PrM0_PrM_corrected_Q_C = res["47928748024346"][0];
+                    self.NP04_DCS_01_PrM0_PrM_corrected_Q_A = res["47928748024602"][0];
+                    self.NP04_DCS_01_PrM0_PrM_Trans_Corr = res["47928748026394"][0];
+                    self.NP04_DCS_01_PrM1_PrM_V_A_Set = res["47928764795674"][0];
+                    self.NP04_DCS_01_PrM1_PrM_V_A = res["47928764796186"][0];
+                    self.NP04_DCS_01_PrM1_PrM_I_A = res["47928764796954"][0];
+                    self.NP04_DCS_01_PrM1_PrM_V_C_Set = res["47928764795930"][0];
+                    self.NP04_DCS_01_PrM1_PrM_V_C = res["47928764796442"][0];
+                    self.NP04_DCS_01_PrM1_PrM_I_C = res["47928764800794"][0];
+                    self.NP04_DCS_01_PrM1_PrM_corrected_charge_ratio = res["47928764793370"][0];
+                    self.NP04_DCS_01_PrM1_PrM_corrected_e_lifetime = res["47928764793626"][0];
+                    self.NP04_DCS_01_PrM1_PrM_Q_C = res["47928764794138"][0];
+                    self.NP04_DCS_01_PrM1_PrM_T_C = res["47928764794394"][0];
+                    self.NP04_DCS_01_PrM1_PrM_BL_C = res["47928764794650"][0];
+                    self.NP04_DCS_01_PrM1_PrM_Q_A = res["47928764794906"][0];
+                    self.NP04_DCS_01_PrM1_PrM_T_A = res["47928764795162"][0];
+                    self.NP04_DCS_01_PrM1_PrM_BL_A = res["47928764795418"][0];
+                    self.NP04_DCS_01_PrM1_PrM_corrected_Q_C = res["47928764801562"][0];
+                    self.NP04_DCS_01_PrM1_PrM_corrected_Q_A = res["47928764801818"][0];
+                    self.NP04_DCS_01_PrM1_PrM_Trans_Corr = res["47928764803610"][0];
+                    self.NP04_DCS_01_PrM2_PrM_V_A_Set = res["47928781572890"][0];
+                    self.NP04_DCS_01_PrM2_PrM_V_A = res["47928781573402"][0];
+                    self.NP04_DCS_01_PrM2_PrM_I_A = res["47928781574170"][0];
+                    self.NP04_DCS_01_PrM2_PrM_V_C_Set = res["47928781573146"][0];
+                    self.NP04_DCS_01_PrM2_PrM_V_C = res["47928781573658"][0];
+                    self.NP04_DCS_01_PrM2_PrM_I_C = res["47928781578010"][0];
+                    self.NP04_DCS_01_PrM2_PrM_corrected_charge_ratio = res["47928781570586"][0];
+                    self.NP04_DCS_01_PrM2_PrM_corrected_e_lifetime = res["47928781570842"][0];
+                    self.NP04_DCS_01_PrM2_PrM_Q_C = res["47928781571354"][0];
+                    self.NP04_DCS_01_PrM2_PrM_T_C = res["47928781571610"][0];
+                    self.NP04_DCS_01_PrM2_PrM_BL_C = res["47928781571866"][0];
+                    self.NP04_DCS_01_PrM2_PrM_Q_A = res["47928781572122"][0];
+                    self.NP04_DCS_01_PrM2_PrM_T_A = res["47928781572378"][0];
+                    self.NP04_DCS_01_PrM2_PrM_BL_A = res["47928781572634"][0];
+                    self.NP04_DCS_01_PrM2_PrM_corrected_Q_C = res["47928781578778"][0];
+                    self.NP04_DCS_01_PrM2_PrM_corrected_Q_A = res["47928781579034"][0];
+                    self.NP04_DCS_01_PrM2_PrM_Trans_Corr = res["47928781580826"][0];
 
-                self.NP04_DCS_01_PrM0_PrM_corrected_charge_ratio = rArr[0];
-                self.NP04_DCS_01_PrM0_PrM_corrected_e_lifetime = rArr[1];
-                self.NP04_DCS_01_PrM0_PrM_Q_C = rArr[2];
-                self.NP04_DCS_01_PrM0_PrM_T_C = rArr[3];
-                self.NP04_DCS_01_PrM0_PrM_BL_C = rArr[4];
-                self.NP04_DCS_01_PrM0_PrM_Q_A = rArr[5];
-                self.NP04_DCS_01_PrM0_PrM_T_A = rArr[6];
-                self.NP04_DCS_01_PrM0_PrM_BL_A = rArr[7];
-                self.NP04_DCS_01_PrM0_PrM_V_A_Set = rArr[8];
-                self.NP04_DCS_01_PrM0_PrM_V_C_Set = rArr[9];
-                self.NP04_DCS_01_PrM0_PrM_V_A = rArr[10];
-                self.NP04_DCS_01_PrM0_PrM_V_C = rArr[11];
-                self.NP04_DCS_01_PrM0_PrM_I_A = rArr[12];
-                self.NP04_DCS_01_PrM0_PrM_I_C = rArr[13];
-                self.NP04_DCS_01_PrM0_PrM_corrected_Q_C = rArr[14];
-                self.NP04_DCS_01_PrM0_PrM_corrected_Q_A = rArr[15];
-                self.NP04_DCS_01_PrM0_PrM_Trans_Corr = rArr[16];
-                self.NP04_DCS_01_PrM1_PrM_corrected_charge_ratio = rArr[17];
-                self.NP04_DCS_01_PrM1_PrM_corrected_e_lifetime = rArr[18];
-                self.NP04_DCS_01_PrM1_PrM_Q_C = rArr[19];
-                self.NP04_DCS_01_PrM1_PrM_T_C = rArr[20];
-                self.NP04_DCS_01_PrM1_PrM_BL_C = rArr[21];
-                self.NP04_DCS_01_PrM1_PrM_Q_A = rArr[22];
-                self.NP04_DCS_01_PrM1_PrM_T_A = rArr[23];
-                self.NP04_DCS_01_PrM1_PrM_BL_A = rArr[24];
-                self.NP04_DCS_01_PrM1_PrM_V_A_Set = rArr[25];
-                self.NP04_DCS_01_PrM1_PrM_V_C_Set = rArr[26];
-                self.NP04_DCS_01_PrM1_PrM_V_A = rArr[27];
-                self.NP04_DCS_01_PrM1_PrM_V_C = rArr[28];
-                self.NP04_DCS_01_PrM1_PrM_I_A = rArr[29];
-                self.NP04_DCS_01_PrM1_PrM_I_C = rArr[30];
-                self.NP04_DCS_01_PrM1_PrM_corrected_Q_C = rArr[31];
-                self.NP04_DCS_01_PrM1_PrM_corrected_Q_A = rArr[32];
-                self.NP04_DCS_01_PrM1_PrM_Trans_Corr = rArr[33];
-                self.NP04_DCS_01_PrM2_PrM_corrected_charge_ratio = rArr[34];
-                self.NP04_DCS_01_PrM2_PrM_corrected_e_lifetime = rArr[35];
-                self.NP04_DCS_01_PrM2_PrM_Q_C = rArr[36];
-                self.NP04_DCS_01_PrM2_PrM_T_C = rArr[37];
-                self.NP04_DCS_01_PrM2_PrM_BL_C = rArr[38];
-                self.NP04_DCS_01_PrM2_PrM_Q_A = rArr[39];
-                self.NP04_DCS_01_PrM2_PrM_T_A = rArr[40];
-                self.NP04_DCS_01_PrM2_PrM_BL_A = rArr[41];
-                self.NP04_DCS_01_PrM2_PrM_V_A_Set = rArr[42];
-                self.NP04_DCS_01_PrM2_PrM_V_C_Set = rArr[43];
-                self.NP04_DCS_01_PrM2_PrM_V_A = rArr[44];
-                self.NP04_DCS_01_PrM2_PrM_V_C = rArr[45];
-                self.NP04_DCS_01_PrM2_PrM_I_A = rArr[46];
-                self.NP04_DCS_01_PrM2_PrM_I_C = rArr[47];
-                self.NP04_DCS_01_PrM2_PrM_corrected_Q_C = rArr[48];
-                self.NP04_DCS_01_PrM2_PrM_corrected_Q_A = rArr[49];
-                self.NP04_DCS_01_PrM2_PrM_Trans_Corr = rArr[50];
 
-                console.log("interval occured");
-                self.timestamp = rArr[rArr.length-1] * 1000;
-            });
+                });
 
-            };
+        };
 
         this.promise;
 
         this.reload();
 
-        $scope.start = function() {
+        $scope.start = function () {
             $scope.stop();
 
             self.promise = $interval(self.reload, 60000);
         };
 
-        $scope.stop = function() {
+        $scope.stop = function () {
             $interval.cancel(self.promise);
         };
         $scope.start();
 
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             $scope.stop();
         });
     }
