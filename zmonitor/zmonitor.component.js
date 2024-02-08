cb80c49dc24de5b83a252600f27f9faceb6ea66c
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-09 03:00:05 +0400

    zmonitor sensor id fix

--- zmonitor/zmonitor.component.js
+++ zmonitor/zmonitor.component.js
@@ -9,27 +9,22 @@ angular.module('zmonitor', []).component('zmonitor', {
 
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=zmonitor").then(function (resultArr) {
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=zmonitor")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.NP04_DCS_01_gizmo_RES = res["47861572043290"][0];
+                    self.NP04_DCS_01_gizmo_TH = res["47861572043546"][0];
+                    self.NP04_DCS_01_gizmo_mag = res["47861572043802"][0];
+                    self.NP04_DCS_01_gizmo_I = res["47861572044058"][0];
+                    self.NP04_DCS_01_gizmo_Q = res["47861572044314"][0];
 
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-
-                self.NP04_DCS_01_gizmo_RES = rArr[0];
-                self.NP04_DCS_01_gizmo_TH = rArr[1];
-                self.NP04_DCS_01_gizmo_mag = rArr[2];
-                self.NP04_DCS_01_gizmo_I = rArr[3];
-                self.NP04_DCS_01_gizmo_Q = rArr[4];
-                self.NP04_MHT0100AI = rArr[5];
-                self.NP04_TT0100AI = rArr[6];
-                self.NP04_PT0106AI = rArr[7];
-
-                console.log("interval occured");
-                self.timestamp = rArr[rArr.length-1] * 1000;
-            });
+                });
         };
 
         this.promise;
