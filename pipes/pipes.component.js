5f3aa1b0eece268eb4effcaf984ac3c9b7aaad86
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-09 02:44:17 +0400

    pipes sensor id fix

--- pipes/pipes.component.js
+++ pipes/pipes.component.js
@@ -7,36 +7,28 @@ angular.module('pipes', []).component('pipes', {
         let self = this;
 
         this.reload = function () {
-
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=pipes").then(function (resultArr) {
-
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-
-                self.NP04_DCS_01_TE0085_ = rArr[3];
-                self.NP04_DCS_01_TE0086_ = rArr[4];
-                self.NP04_DCS_01_TE0087_ = rArr[5];
-                self.NP04_DCS_01_TE0088_ = rArr[6];
-                self.NP04_DCS_01_TE0089_ = rArr[7];
-                self.NP04_DCS_01_TE0090_ = rArr[8];
-                self.NP04_DCS_01_TE0091_ = rArr[9];
-                self.NP04_DCS_01_TE0092_ = rArr[10];
-                self.NP04_DCS_01_TE0093_ = rArr[11];
-                self.NP04_DCS_01_TE0094_ = rArr[12];
-                self.NP04_DCS_01_TE0095_ = rArr[13];
-                self.NP04_DCS_01_TE0096_ = rArr[14];
-
-                console.log("interval occured");
-                self.timestamp = rArr[rArr.length-1] * 1000;
-            });
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=pipes")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.NP04_DCS_01_TE0085_ = res["47891670368538"][0];
+                    self.NP04_DCS_01_TE0086_ = res["47891687145754"][0];
+                    self.NP04_DCS_01_TE0087_ = res["47891703922970"][0];
+                    self.NP04_DCS_01_TE0088_ = res["47891720700186"][0];
+                    self.NP04_DCS_01_TE0089_ = res["47891737477402"][0];
+                    self.NP04_DCS_01_TE0090_ = res["47891754254618"][0];
+                    self.NP04_DCS_01_TE0091_ = res["47891771031834"][0];
+                    self.NP04_DCS_01_TE0092_ = res["47891787809050"][0];
+                    self.NP04_DCS_01_TE0093_ = res["47891804586266"][0];
+                    self.NP04_DCS_01_TE0094_ = res["47891821363482"][0];
+                    self.NP04_DCS_01_TE0095_ = res["47891838140698"][0];
+                    self.NP04_DCS_01_TE0096_ = res["47891854917914"][0];
+                });
 
         };
 
@@ -44,18 +36,18 @@ angular.module('pipes', []).component('pipes', {
 
         this.reload();
 
-        $scope.start = function() {
+        $scope.start = function () {
             $scope.stop();
 
             self.promise = $interval(self.reload, 60000);
         };
 
-        $scope.stop = function() {
+        $scope.stop = function () {
             $interval.cancel(self.promise);
         };
         $scope.start();
 
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             $scope.stop();
         });
     }
