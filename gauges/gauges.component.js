1b27cc6f88a18a2e55d333fb28a58075294ffafb
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 22:30:04 +0400

    gauges js

--- gauges/gauges.component.js
+++ gauges/gauges.component.js
@@ -4,44 +4,34 @@ angular.module('gauges', []).component('gauges', {
     controller: function gaugesController($scope, $http, $interval) {
         this.pageTitle = "NP04 Gauges";
         this.natalie = 1;
-        let self = this;
+        const self = this;
 
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=gauges").then(function (resultArr) {
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=gauges")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                });
 
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-                
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                console.log("interval occured");
-                self.timestamp = rArr[rArr.length-1] * 1000;
+            this.promise;
+            this.reload();
+            $scope.start = function () {
+                $scope.stop();
+
+                self.promise = $interval(self.reload, 60000);
+            };
+            $scope.stop = function () {
+                $interval.cancel(self.promise);
+            };
+            $scope.start();
+            $scope.$on('$destroy', function () {
+                $scope.stop();
             });
-        };
-
-        this.promise;
-
-        this.reload();
-
-        $scope.start = function() {
-            $scope.stop();
-
-            self.promise = $interval(self.reload, 60000);
-        };
-
-        $scope.stop = function() {
-            $interval.cancel(self.promise);
-        };
-        $scope.start();
-
-        $scope.$on('$destroy', function() {
-            $scope.stop();
-        });
-    }
-});
+        }
+    });
