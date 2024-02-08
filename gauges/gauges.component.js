e036e1ad5685cdd0f26fd1c9c3b010e00784a401
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 22:44:31 +0400

    gauges js

--- gauges/gauges.component.js
+++ gauges/gauges.component.js
@@ -5,7 +5,6 @@ angular.module('gauges', []).component('gauges', {
         this.pageTitle = "NP04 Gauges";
         this.natalie = 1;
         const self = this;
-
         this.reload = function () {
 
             self.timestamp = new Date();
@@ -18,21 +17,21 @@ angular.module('gauges', []).component('gauges', {
                     self.NP04_TT0100AI = res["47878802266906"][0];
                     self.NP04_PT0106AI = res["47878819044122"][0];
                 });
-
-            this.promise;
-            this.reload();
-            $scope.start = function () {
-                $scope.stop();
-
-                self.promise = $interval(self.reload, 60000);
-            };
-            $scope.stop = function () {
-                $interval.cancel(self.promise);
-            };
-            $scope.start();
-            $scope.$on('$destroy', function () {
-                $scope.stop();
-            });
         }
+
+        this.promise;
+        this.reload();
+        $scope.start = function () {
+            $scope.stop();
+
+            self.promise = $interval(self.reload, 60000);
+        };
+        $scope.stop = function () {
+            $interval.cancel(self.promise);
+        };
+        $scope.start();
+        $scope.$on('$destroy', function () {
+            $scope.stop();
+        });
     }
 });
