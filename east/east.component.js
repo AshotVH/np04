1baacb2b9b3e7317ad61f4e4c65817328ad14923
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 23:01:37 +0400

    east js

--- east/east.component.js
+++ east/east.component.js
@@ -5,31 +5,25 @@ angular.module('east', []).component('east', {
         this.pageTitle = "NP04 Saleve side GTT probes";
         this.natalie = 1;
         this.TT0101 = "";
-        let self = this;
+        const self = this;
 
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=east").then(function (resultArr) {
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=east")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.NP04_DCS_01_TE0127 = res["47892375011610"][0];
+                });
 
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-                
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                self.NP04_DCS_01_TE0127 = rArr[3];
-
-                console.log("interval occured");
-            self.timestamp = rArr[rArr.length-1] * 1000;
-            });
         };
 
         this.promise;
-
         this.reload();
 
         $scope.start = function() {
