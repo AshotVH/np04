29e09f9c00a19e7409743f3c8d410b35600e4db2
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 22:57:53 +0400

    north js

--- north/north.component.js
+++ north/north.component.js
@@ -5,45 +5,41 @@ angular.module('north', []).component('north', {
         this.pageTitle = "NP04 Lausanne side GTT probes";
         this.natalie = 1;
         this.TT0101 = "";
-        let self = this;
-
+        const self = this;
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=north").then(function (resultArr) {
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=north")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.NP04_DCS_01_TE0122 = res["47892291125530"][0];
 
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                self.NP04_DCS_01_TE0122 = rArr[3];
+                });
 
-                console.log("interval occured");
-                self.timestamp = rArr[rArr.length-1] * 1000;
-            });
         };
 
         this.promise;
-
         this.reload();
 
-        $scope.start = function() {
+        $scope.start = function () {
             $scope.stop();
-
             self.promise = $interval(self.reload, 60000);
         };
 
-        $scope.stop = function() {
+        $scope.stop = function () {
             $interval.cancel(self.promise);
         };
+
         $scope.start();
 
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             $scope.stop();
         });
+
     }
 });
