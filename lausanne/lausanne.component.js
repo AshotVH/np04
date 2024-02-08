3765594e7a554713fde127dbbd4b66d55e02d9c6
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 22:47:35 +0400

    lausanne js

--- lausanne/lausanne.component.js
+++ lausanne/lausanne.component.js
@@ -4,49 +4,41 @@ angular.module('lausanne', []).component('lausanne', {
     controller: function lausanneController($scope, $http, $q, $interval) {
         this.pageTitle = "NP04 Lausanne side";
         this.natalie = 1;
-        let self = this;
+        const self = this;
 
         this.reload = function () {
 
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=lausanne").then(function (resultArr) {
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                self.MC_LJS_03 = rArr[3];
-                self.MC_LJS_02 = rArr[4];
-                self.TC_LS_02 = rArr[5];
-                self.BC_LS_03 = rArr[6];
-                self.B3_LS_01 = rArr[7];
-                self.MEM_LS_03 = rArr[8];
-                self.MEM_LS_02 = rArr[9];
-                self.DS_LS_01 = rArr[10];
-            self.timestamp = rArr[rArr.length-1] * 1000;
-            });
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=lausanne")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.DS_LS_01 = res["47884355525402"][0];
+                    self.TC_LS_02 = res["47884238084890"][0];
+                    self.B3_LS_01 = res["47884305193754"][0];
+                    self.BC_LS_03 = res["47884271639322"][0];
+                    self.MEM_LS_03 = res["47884321970970"][0];
+                    self.MEM_LS_02 = res["47884338748186"][0];
+                    self.MC_LJS_03 = res["47884187753242"][0];
+                    self.MC_LJS_02 = res["47884221307674"][0];
+                });
         };
 
         this.promise;
-
         this.reload();
-
-        $scope.start = function() {
+        $scope.start = function () {
             $scope.stop();
-
             self.promise = $interval(self.reload, 150000);
         };
-
-        $scope.stop = function() {
+        $scope.stop = function () {
             $interval.cancel(self.promise);
         };
         $scope.start();
-
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             $scope.stop();
         });
     }
