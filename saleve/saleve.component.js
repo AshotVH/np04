3a29ac26c19f5d601f141d1850f4bc14dd41022a
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-08 22:53:22 +0400

    saleve js

--- saleve/saleve.component.js
+++ saleve/saleve.component.js
@@ -4,58 +4,49 @@ angular.module('saleve', []).component('saleve', {
     controller: function saleveController($scope, $http, $q, $interval) {
         this.pageTitle = "NP04 Saleve";
         this.natalie = 1;
-        let self = this;
+        const self = this;
 
         this.reload = function () {
-
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=saleve").then(function (resultArr) {
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                self.MC_LSS_04 = rArr[3];
-                self.MC_LSS_03 = rArr[4];
-                self.MC_LSS_01 = rArr[5];
-                self.MC_LSS_02 = rArr[6];
-                self.TC_SS_02 = rArr[7];
-                self.TC_SS_01 = rArr[8];
-                self.B4_SS_01 = rArr[9];
-                self.B3_SS_01 = rArr[10];
-                self.B2_SS_01 = rArr[11];
-                self.TC_SS_03 = rArr[12];
-                self.MEM_SS_02 = rArr[13];
-                self.DS_SS_01 = rArr[14];
-                self.BC_SS_04 = rArr[15];
-                self.BC_SS_03 = rArr[16];
-                self.BC_SS_01 = rArr[17];
-                self.MEM_SS_03 = rArr[18];
-                self.MEM_SS_01 = rArr[19];
-            self.timestamp = rArr[rArr.length-1] * 1000;
-            });
+            self.timestamp = new Date();
+            $http
+                .get("php-db-conn/np04cachedvals.php?elemName=saleve")
+                .then(function (result) {
+                    const res = result.data;
+                    console.log(res);
+                    self.NP04_MHT0100AI = res["47878785489690"][0];
+                    self.NP04_TT0100AI = res["47878802266906"][0];
+                    self.NP04_PT0106AI = res["47878819044122"][0];
+                    self.B4_SS_01 = res["47884573629210"][0];
+                    self.DS_SS_01 = res["47884707846938"][0];
+                    self.TC_SS_01 = res["47884540074778"][0];
+                    self.TC_SS_02 = res["47884506520346"][0];
+                    self.TC_SS_03 = res["47884623960858"][0];
+                    self.B3_SS_01 = res["47884590406426"][0];
+                    self.B2_SS_01 = res["47884607183642"][0];
+                    self.BC_SS_01 = res["47884808510234"][0];
+                    self.BC_SS_03 = res["47884791733018"][0];
+                    self.BC_SS_04 = res["47884741401370"][0];
+                    self.MEM_SS_03 = res["47884875619098"][0];
+                    self.MEM_SS_02 = res["47884657515290"][0];
+                    self.MEM_SS_01 = res["47884892396314"][0];
+                    self.MC_LSS_04 = res["47884389079834"][0];
+                    self.MC_LSS_03 = res["47884405857050"][0];
+                    self.MC_LSS_02 = res["47884489743130"][0];
+                    self.MC_LSS_01 = res["47884439411482"][0];
+                });
         };
 
         this.promise;
-
         this.reload();
-
-        $scope.start = function() {
+        $scope.start = function () {
             $scope.stop();
-
             self.promise = $interval(self.reload, 150000);
         };
-
-        $scope.stop = function() {
+        $scope.stop = function () {
             $interval.cancel(self.promise);
         };
         $scope.start();
-
-        $scope.$on('$destroy', function() {
+        $scope.$on('$destroy', function () {
             $scope.stop();
         });
     }
