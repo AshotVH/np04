26ddff1ea00739f5840d6fea688586d543f67d7e
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-13 06:15:11 +0400

    bellegarde js

--- bellegarde/bellegarde.component.js
+++ bellegarde/bellegarde.component.js
@@ -1,55 +1,51 @@
-'use strict';
-angular.module('bellegarde', []).component('bellegarde', {
-    templateUrl: 'bellegarde/bellegarde.template.html',
-    controller: function bellegardeController($scope, $http, $q, $interval) {
-        this.pageTitle = "NP04 Bellegarde side";
-        this.natalie = 1;
-        let self = this;
+"use strict";
+angular.module("bellegarde", []).component("bellegarde", {
+  templateUrl: "bellegarde/bellegarde.template.html",
+  controller: function bellegardeController($scope, $http, $q, $interval) {
+    this.pageTitle = "NP04 Bellegarde side";
+    this.natalie = 1;
+    let self = this;
 
-        this.reload = function () {
-
-            $http.get("php-db-conn/cachedVals.conn.php?elemId=bellegarde").then(function (resultArr) {
-                let rArr = [];
-                let resjson = angular.toJson(resultArr.data);
-                let res = JSON.parse(resjson);
-                for (let i = 0; i < res.length; i++) {
-                    rArr.push(JSON.parse(res[i]));
-                }
-
-                self.NP04_MHT0100AI = rArr[0];
-                self.NP04_TT0100AI = rArr[1];
-                self.NP04_PT0106AI = rArr[2];
-                self.BC_BS_03 = rArr[3];
-                self.MEM_BS_03 = rArr[4];
-                self.MEM_BS_02 = rArr[5];
-                self.B4_BS_01 = rArr[6];
-                self.B3_BS_01 = rArr[7];
-                self.B2_BS_01 = rArr[8];
-                self.TCO_BS_01 = rArr[9];
-                self.DS_BS_01 = rArr[10];
-                self.MC_BSS_03 = rArr[11];
-                self.MC_BSS_02 = rArr[12];
-                self.timestamp = rArr[rArr.length-1] * 1000;
-            });
-        };
-
-        this.promise;
-
-        this.reload();
-
-        $scope.start = function() {
-            $scope.stop();
-
-            self.promise = $interval(self.reload, 30000);
-        };
-
-        $scope.stop = function() {
-            $interval.cancel(self.promise);
-        };
-        $scope.start();
-
-        $scope.$on('$destroy', function() {
-            $scope.stop();
+    this.reload = function () {
+      $http
+        .get("php-db-conn/np04cachedvals.php?elemName=bellegarde")
+        .then(function (result) {
+          self.timestamp = new Date();
+          const res = result.data;
+          console.log(res);
+          self.B4_BS_01 = res["47883902540570"]?res["47883902540570"][0]:"N/A";
+          self.DS_BS_01 = res["47883969649434"]?res["47883969649434"][0]:"N/A";
+          self.TCO_BS_01 = res["47883952872218"]?res["47883952872218"][0]:"N/A";
+          self.B3_BS_01 = res["47883919317786"]?res["47883919317786"][0]:"N/A";
+          self.B2_BS_01 = res["47883936095002"]?res["47883936095002"][0]:"N/A";
+          self.MEM_BS_03 = res["47883868986138"]?res["47883868986138"][0]:"N/A";
+          self.MEM_BS_02 = res["47883885763354"]?res["47883885763354"][0]:"N/A";
+          self.BC_BS_03 = res["47883835431706"]?res["47883835431706"][0]:"N/A";
+          self.MC_BSS_03 = res["47883986426650"]?res["47883986426650"][0]:"N/A";
+          self.MC_BSS_02 = res["47884019981082"]?res["47884019981082"][0]:"N/A";
+          self.NP04_MHT0100AI = res["47878785489690"]?res["47878785489690"][0]:"N/A";
+          self.NP04_TT0100AI = res["47878802266906"]?res["47878802266906"][0]:"N/A";
+          self.NP04_PT0106AI = res["47878819044122"]?res["47878819044122"][0]:"N/A";   
         });
-    }
+    };
+
+    this.promise;
+
+    this.reload();
+
+    $scope.start = function () {
+      $scope.stop();
+
+      self.promise = $interval(self.reload, 30000);
+    };
+
+    $scope.stop = function () {
+      $interval.cancel(self.promise);
+    };
+    $scope.start();
+
+    $scope.$on("$destroy", function () {
+      $scope.stop();
+    });
+  },
 });
