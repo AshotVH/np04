565e666a669c768e5b612202fa4787a680512480
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-18 19:51:45 +0400

    zmonitor

--- zmonitor/zmonitor.component.js
+++ zmonitor/zmonitor.component.js
@@ -5,7 +5,7 @@ angular.module('zmonitor', []).component('zmonitor', {
         this.pageTitle = "NP04 Ground impedance monitor";
         this.natalie = 1;
         this.TT0101 = "";
-        let self = this;
+        const self = this;
 
         this.reload = function () {
 
@@ -15,15 +15,14 @@ angular.module('zmonitor', []).component('zmonitor', {
                 .then(function (result) {
                     const res = result.data;
                     console.log(res);
-                    self.NP04_MHT0100AI = res["47878785489690"][0];
-                    // self.NP04_TT0100AI = res["47878802266906"][0];
-                    self.NP04_PT0106AI = res["47878819044122"][0];
-                    self.NP04_DCS_01_gizmo_RES = res["47861572043290"][0];
-                    self.NP04_DCS_01_gizmo_TH = res["47861572043546"][0];
-                    self.NP04_DCS_01_gizmo_mag = res["47861572043802"][0];
-                    self.NP04_DCS_01_gizmo_I = res["47861572044058"][0];
-                    self.NP04_DCS_01_gizmo_Q = res["47861572044314"][0];
-
+                    self.NP04_DCS_01_gizmo_RES = res["47861572043290"]?res["47861572043290"][0]:"N/A";
+                    self.NP04_DCS_01_gizmo_TH = res["47861572043546"]?res["47861572043546"][0]:"N/A";
+                    self.NP04_DCS_01_gizmo_mag = res["47861572043802"]?res["47861572043802"][0]:"N/A";
+                    self.NP04_DCS_01_gizmo_I = res["47861572044058"]?res["47861572044058"][0]:"N/A";
+                    self.NP04_DCS_01_gizmo_Q = res["47861572044314"]?res["47861572044314"][0]:"N/A";
+                    self.NP04_MHT0100AI = res["47878785489690"]?res["47878785489690"][0]:"N/A";
+                    self.NP04_TT0100AI = res["47878802266906"]?res["47878802266906"][0]:"N/A";
+                    self.NP04_PT0106AI = res["47878819044122"]?res["47878819044122"][0]:"N/A";
                 });
         };
 
