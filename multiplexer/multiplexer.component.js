720cf021ddc38d8fe704780e82d86e092b855612
Ashot Hambardzumyan <avhmail@gmail.com>
2024-02-18 18:01:53 +0400

    multiplexer

--- multiplexer/multiplexer.component.js
+++ multiplexer/multiplexer.component.js
@@ -69,7 +69,7 @@ angular.module("multiplexer", []).component("multiplexer", {
 
       this.reload = function () {
         $http
-          .get("php-db-conn/cachedVals.conn.php?elemId=multiplexer1")
+          .get("php-db-conn/np04cachedvals.conn.php?elemId=multiplexer1")
           .then(function (result) {
             const res = result.data;
             console.log(res);
