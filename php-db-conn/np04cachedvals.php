7272f78a365c92be6d3a18b62c7a97cfdd44d03e
Ashot Hambardzumyan <avhmail@gmail.com>
2025-07-03 12:27:58 +0400

    env

--- php-db-conn/np04cachedvals.php
+++ php-db-conn/np04cachedvals.php
@@ -4,7 +4,8 @@ ini_set('memory_limit', '6G');
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
-$hostName = $_ENV["hostname"];
+// $hostName = $_ENV["hostname"];
+$hostName = getenv('hostname');
 $elemName = $_GET["elemName"];
 $url = $hostName."/latest/".$elemName;
 $json = json_decode(file_get_contents($url));
