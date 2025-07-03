f01b8a79c9085987533f0e84599408a43a0200d0
Ashot Hambardzumyan <avhmail@gmail.com>
2025-07-03 12:36:35 +0400

    to getenv

--- php-db-conn/np04cachedvals.php
+++ php-db-conn/np04cachedvals.php
@@ -4,7 +4,6 @@ ini_set('memory_limit', '6G');
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
 
-// $hostName = $_ENV["hostname"];
 $hostName = getenv('hostname');
 $elemName = $_GET["elemName"];
 $url = $hostName."/latest/".$elemName;
